add67f0b853de7543c4bfc7493908802474c36b9
oceanfly <805651381@qq.com>
2024-11-27 11:24:06 +0800

    1.添加谷歌支付

---
+++ vue/google-pay-vue/README.md
@@ -0,0 +1,21 @@
+# Google pay button example
+
+Integrates google pay with vue
+
+## Where's it integrated?
+
+`src/components/GooglePay.vue`, there you can see all the implementation.<br />
+If you're using nuxt or some other platform, see how to add the google script to the head (The script in this project is in `public/index.html`).
+
+## Pre-requisites
+
+- node 10.x +
+- npm
+
+## Running
+
+1. On root of the folder create a archive `.env` with what's on `.env.example`
+2. If you want to test GooglePay on production, just change what's on `.env` with your production setups
+3. Install the dependencies wth `yarn` or `npm i`
+4. Run `yarn serve` or `npm run serve` to run the server
+5. Open your [localhost](http://localhost:8080/)
---
+++ vue/google-pay-vue/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1>
+      Botão google pay
+    </h1>
+    <GooglePay
+      totalPrice="100.00"
+      @loaded="loaded"
+      @loadedError="loadedError"
+      @paymentError="paymentError"
+      @paymentSuccess="paymentSuccess" />
+  </div>
+</template>
+
+<script>
+import GooglePay from './components/GooglePay.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GooglePay,
+  },
+  methods: {
+    loaded(data) {
+      console.log('loaded: ', data);
+    },
+    loadedError(data) {
+      console.log('loadedError: ', data);
+    },
+    paymentError(data) {
+      console.log('paymentError: ', data);
+    },
+    paymentSuccess(data) {
+      console.log('paymentSuccess: ', data);
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

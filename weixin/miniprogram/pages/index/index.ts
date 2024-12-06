877953336adc701195bdd5db7c896e07886096ed
oceanfly <805651381@qq.com>
2024-12-06 17:42:59 +0800

    1.cm

--- weixin/miniprogram/pages/index/index.ts
+++ weixin/miniprogram/pages/index/index.ts
@@ -52,6 +52,7 @@ Component({
       })
     },
     onInputChange(e: any) {
+      console.log(111111)
       const nickName = e.detail.value
       const { avatarUrl } = this.data.userInfo
       this.setData({
@@ -60,6 +61,7 @@ Component({
       })
     },
     getUserProfile() {
+      console.log(222222)
       // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
       wx.getUserProfile({
         desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
